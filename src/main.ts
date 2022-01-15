e960630537d54a49e73e1aa0f399cd4abd741e10
Ash Porter <ashley.porter@eyecuelab.com>
2022-01-15 15:23:29 -0800

    more linter rules, add chalk

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import chalk = require('chalk');
+
+// ```
+// import chalk = require('chalk');
+
+// log(chalk.red.bgBlack`2 + 3 = {bold ${2 + 3}}`)
+// ```
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`app running at`, chalk.red.bgBlack`${process.env.PORT}`);
 }
 bootstrap();
