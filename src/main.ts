b6d916f5920d608a5ccddd805dc60c1da0ee9a5a
Ash Porter <ashley.porter@eyecuelab.com>
2022-01-15 21:42:56 -0800

    minor tweaks to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import chalk = require('chalk');
 
-// ```
-// import chalk = require('chalk');
-
-// log(chalk.red.bgBlack`2 + 3 = {bold ${2 + 3}}`)
-// ```
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`app running at`, chalk.red.bgBlack`${process.env.PORT}`);
+  console.log(`app running on port`, chalk.red.bgBlack`${process.env.PORT}`);
 }
 bootstrap();
