e570edd141fd5f82aae16850584d1a8f67bd146a
Ash Porter <ashley.porter@eyecuelab.com>
2022-01-25 23:19:01 -0800

    finish up authguards and update profile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { UserService } from './user/user.service';
 
 @Controller('app')
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private authService: AuthService,
+    private userService: UserService,
   ) {}
 
   @Get()
@@ -19,5 +22,17 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    const user = await this.userService.findOne({
+      where: { username: req.user.username },
+    });
+    if (user) {
+      const { password, ...rest } = user;
+      return rest;
+    }
   }
 }
